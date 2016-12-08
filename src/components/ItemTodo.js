8460d32d59c6d53abf8e37954124006b5b2a4a62
Chaiwat Tungtongsoontorn <chaiwat@sanook.com>
2016-12-08 10:47:13 +0700

    complete edit todo

--- src/components/ItemTodo.js
+++ src/components/ItemTodo.js
@@ -1,14 +1,21 @@
 import React, { Component } from 'react'
+import firebase from '../lib/firebase'
 
 export default class ItemTodo extends Component {
     handleClick = (item) => (e) => {
         let { removeList } = this.props
         removeList(item)
     }
+
+    handlerOnChange = (key) => (e) => {
+        var updates = {}
+        updates[`/todos/${key}`] = {name: e.target.value}
+        firebase.database().ref().update(updates)
+    }
     
     render() {
         let { item } = this.props
 
-        return <li>{item.name} <button onClick={this.handleClick(item)}>x</button></li>
+        return <li><input type="text" onChange={this.handlerOnChange(item.key)} value={item.name} /> <button onClick={this.handleClick(item)}>x</button></li>
     }
 }
